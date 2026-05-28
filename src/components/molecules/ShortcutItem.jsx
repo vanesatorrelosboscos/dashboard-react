945b93ec8a8217a911828ac9aadec4208179fd52
vanesa <vanesa.torre@salesianoslosboscos.com>
2026-05-28 17:16:21 +0200

    migrate ShortcutItemm css to tailwind

--- src/components/molecules/ShortcutItem.jsx
+++ src/components/molecules/ShortcutItem.jsx
@@ -1,10 +1,13 @@
-function ShortCutsItem({action, keys}){
-    return(
-        <div className="shortcut-item">
+function ShortCutsItem({ action, keys }) {
+    return (
+        <div className="flex justify-between items-center text-[12px] bg-(--surface2) rounded-(--radius) p-3">
             <span>{action}</span>
-            <span className="shortcut-keys">
-                {keys.map(key => (
-                    <span key={crypto.randomUUID()} className="kbd">
+            <span>
+                {keys.map((key, index) => (
+                    <span 
+                        key={index} 
+                        className="bg-(--surface2) py-0.5 px-1.5 rounded-[4px] border border-solid border-(--border) text-(--muted) text-[11px] ml-1 inline-block"
+                    >
                         {key}
                     </span>
                 ))}
