c62a9f7d2014ecebf6d0d5d63acbd0681e9fc803
vanesa <vanesa.torre@salesianoslosboscos.com>
2026-05-28 12:30:24 +0200

    Migrate Avatar component to tailwind v4

--- src/components/atoms/Avatar.jsx
+++ src/components/atoms/Avatar.jsx
@@ -1,5 +1,5 @@
 function Avatar({name}){
-    return <div className="avatar" id="avatarBtn">{name}</div>
+    return <div className="flex size-9.5 shrink-0 bg-linear-to-br from-(--primary) to-(--accent) justify-center items-center rounded-full font-extrabold text-sm text-white border-2 border-solid border-(--primary) cursor-pointer">{name}</div>
 }
 
 export default Avatar
