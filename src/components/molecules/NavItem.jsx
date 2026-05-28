d5eb7e82c83a4d9539fd4e4facb9787c5a09c5c3
vanesa <vanesa.torre@salesianoslosboscos.com>
2026-05-28 17:06:30 +0200

    migrate NavItem css to tailwind

--- src/components/molecules/NavItem.jsx
+++ src/components/molecules/NavItem.jsx
@@ -1,15 +1,29 @@
 function NavItem({ isActive, icon, title, badge, badgeColor }) {
+    const navClass = `flex items-center rounded-(--radius) transition-(--transition) text-[14px] h-[40.5px] w-[227px] py-2 px-3.5 font-medium cursor-pointer hover:bg-(--surface2) hover:text-(--text) ${
+        isActive 
+            ? "bg-[rgba(79,70,229,0.15)] text-(--primary)" 
+            : "text-(--muted)"
+    }`
+
+    const badgeColors = {
+        'green': 'bg-(--success)'
+    }
+    const currentBadgeBg = badgeColors[badgeColor] || 'bg-(--primary)'
+
     return (
-        <a className={isActive ? "nav-item active" : "nav-item"}>
-            <span className="nav-icon">{icon}</span> {title}
+        <a className={navClass}>
+            <span className="size-5 inline-flex justify-center items-center mr-2.5">
+                {icon}
+            </span> 
+            {title}
             
             {badge && (
-                <span className={`nav-badge ${badgeColor ? badgeColor : ""}`}>
+                <span className={`${currentBadgeBg} text-white text-[10px] py-0.5 px-1.5 rounded-[20px] ml-auto font-bold w-5.25 h-4 flex justify-center items-center`}>
                     {badge}
                 </span>
             )}
         </a>
-    );
+    )
 }
 
 export default NavItem
